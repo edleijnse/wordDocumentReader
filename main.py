a3a517add4b1128349c67662a274968a47c1767e
Ed Leijnse <ed@leijnse.info>
2022-03-24 11:55:57 +0100

    words frequency summary

--- main.py
+++ main.py
@@ -3,17 +3,18 @@ from docx2python import docx2python
 from collections import Counter
 
 import os
+import pandas as pd
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 def extract_words(filename, outputFile):
     print("---------------------------------------------------------------------------")
-    outputFile.write("-------------------------------------------------------------------\n")
+    outputFile.write("\n------------------------------------------------------------------------------------------------\n")
     print(filename)
     outputFile.write(filename+"\n")
     print("---------------------------------------------------------------------------")
-    outputFile.write("-------------------------------------------------------------------\n")
+    outputFile.write("------------------------------------------------------------------------------------------------\n")
     doc_result = docx2python(filename).text
     print(doc_result)
     outputFile.writelines(doc_result)
@@ -22,9 +23,13 @@ def extract_words(filename, outputFile):
     # extract words in a table
     res = doc_result.split()
     return res
-def print_tallied(tallied, outputFile):
-    sorted_tallied = sorted(tallied.items())
+def print_tallied(sorted_tallied, outputFile, title):
     #print(sorted_tallied)
+    outputFile.write(
+        "\n----------------------------------------------------------------------------------------------\n")
+    outputFile.write(title)
+    outputFile.write(
+        "\n----------------------------------------------------------------------------------------------\n")
     ii = 0
     for item in sorted_tallied:
         if (item[1]>0):
@@ -33,8 +38,33 @@ def print_tallied(tallied, outputFile):
               outputFile.write(str(item[0]) + ":" + str(item[1]) + "\n")
               ii = ii + 1
     print("total: " + str(ii))
+
     outputFile.write("total: " + str(ii))
 
+def sort_panda(tallied, outputFile):
+    outputFile.write(
+        "\n----------------------------------------------------------------------------------------------\n")
+    outputFile.write("summary words by frequency")
+    outputFile.write(
+        "\n----------------------------------------------------------------------------------------------\n")
+
+    ii = 0
+    wordTab = []
+    countTab = []
+
+    for item in tallied:
+        if (item[1] > 0):
+            if (item[0].isalpha()):
+                wordTab.append(item[0])
+                countTab.append(item[1])
+                print(str(item[0]) + ":" + str(item[1]))
+                ii = ii + 1
+    data = {'Count': countTab, 'Word': wordTab}
+    df = pd.DataFrame(data, columns=['Count', 'Word'])
+    df.sort_values(by=['Count','Word'], inplace=True)
+    for oneValue in df.values:
+        print(str(oneValue[0]) + " " + oneValue[1])
+        outputFile.write(str(oneValue[0]) + " " + oneValue[1] + "\n")
 
 def word_document_reader(inputDir, outputFile):
     # Use a breakpoint in the code line below to debug your script.
@@ -48,7 +78,9 @@ def word_document_reader(inputDir, outputFile):
             if ("doc" in filename):
                 total_res = total_res + extract_words(filename, outputFile)
     tallied = Counter(total_res)
-    print_tallied(tallied, outputFile)
+    sorted_tallied = sorted(tallied.items())
+    print_tallied(sorted_tallied, outputFile, "summary of words")
+    sort_panda(tallied.items(),outputFile)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
